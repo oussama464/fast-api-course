49a6e0ca2a2a93ce00b8c3d5f3b68aea04b96bc8
Oussama MISSAOUI <oussama.missaoui201@gmail.com>
2024-09-27 15:42:43 +0200

    prepare a test db

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,27 @@
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from app import schemas
+from app.config import settings
 from app.main import app
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TetsingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def override_get_db():
+    db = TetsingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 client = TestClient(app)
 
@@ -17,4 +37,4 @@ def test_create_user():
     res = client.post("/users/", json={"email": "hello123@gmail.com", "password": "pass123"})
     result = schemas.UserOut(**res.json())
     print(result)
-    # assert res.status_code == 201
+    assert res.status_code == 201
