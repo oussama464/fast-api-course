d5bf0eefddddb713ac1b26919f9819f0cb1d1621
Oussama MISSAOUI <oussama.missaoui201@gmail.com>
2024-09-27 16:42:35 +0200

    fix test

--- app/config.py
+++ app/config.py
@@ -1,8 +1,12 @@
+import pathlib
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ENV_FILE = pathlib.Path(__file__).parent.parent.joinpath(".env")
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=ENV_FILE, env_file_encoding="utf-8")
     database_hostname: str
     database_port: str
     database_password: str
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,18 +1,18 @@
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
-from app import schemas
+from app import models, schemas
 from app.config import settings
+from app.database import get_db
 from app.main import app
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}_test"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 TetsingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+models.Base.metadata.create_all(bind=engine)
 
 
 def override_get_db():
@@ -22,6 +22,8 @@ def override_get_db():
     finally:
         db.close()
 
+
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
